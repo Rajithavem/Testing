572e5f62bf14bc27ac00ca10821a1919ca2e5884
RajithaV <rajitha.siripuram@gmail.com>
2022-05-11 00:25:09 -0700

    Added new print statements

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,45 @@
-package org.example;public class Main {
+package org.example;
+
+import java.time.LocalDateTime;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+     System.out.println("learning");
+     System.out.println(100+200);
+        System.out.println("100+200="+(100+200));
+        System.out.println(4490-621);
+        System.out.println("4490-621="+(4490-621));
+        System.out.println("current datetime: "+ LocalDateTime.now());
+        int number1 = 100;
+        int number2 = 200;
+
+        int numberTotal = number1+number2;
+        int total = 350;
+        if(total==numberTotal)
+        {
+            System.out.println("equal");
+        }
+        else if (total>numberTotal)
+        {
+            System.out.println(">");
+        }
+        else if (total<numberTotal)
+        {
+            System.out.println(",");
+        }
+        else
+        {
+            System.out.println("none of the above");
+        }
+        boolean x = true;
+        boolean y = false;
+        if(x==y){
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+
+        }
     }
 }
